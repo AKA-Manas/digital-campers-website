c080d83964a3883cad59670ffb7b53d3cdb290c6
Manas <suryawanshimanas888@gmail.com>
2022-11-21 20:20:19 +0530

    first commit design ready

---
+++ contact/index.js
@@ -0,0 +1,6 @@
+const handleClick = () => {
+  const NAME = document.getElementById("name").value;
+  const EMAIL = document.getElementById("email").value;
+  const PHONE = document.getElementById("phone").value;
+  const MESSAGE = document.getElementById("message").value;
+};
